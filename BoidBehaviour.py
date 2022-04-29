2ec10f463585018ee669b2ce2817aa79387c75e8
JonKramme <86349628+JonKramme@users.noreply.github.com>
2022-04-29 10:13:09 +0200

    perceived Center needs to be initialized as (0,0) as we have to add every boids position to the initial value. That means we have to check for (0,0) value. initial Separation commit

--- BoidBehaviour.py
+++ BoidBehaviour.py
@@ -1,21 +1,25 @@
 import VectorMath as vm
-def coherence(position,visibleBoids, coherenceFactor):
+def coherence(centerBoidPosition, visibleBoids, coherenceFactor):
     # Calculate the center position of all "visible" boids except this one, calculate direction vector
     # towards that point and scale it by the coherence Factor. Then return it
-    perceivedCenter = None
+    perceivedCenter = (0,0)
     for boid in visibleBoids:
         perceivedCenter = vm.addVector2(perceivedCenter, boid.position)
     if len(visibleBoids) > 1:
         perceivedCenter = (x / len(visibleBoids) for x in perceivedCenter) #divides all tuple elements by the length
-    if perceivedCenter != None: # We need to check for "change" to avoid tendency towards the top left. (negative our position)
-        perceivedCenter = vm.subVector2(perceivedCenter, position)
+    if perceivedCenter != (0,0): # We need to check for "change" to avoid tendency towards the top left. (negative our position)
+        perceivedCenter = vm.subVector2(perceivedCenter, centerBoidPosition)
 
     perceivedCenter = (x / coherenceFactor for x in perceivedCenter) #divides all tuple elements by the coherence factor
     return perceivedCenter
 
 
-def separation( tooCloseBoids):
+def separation(centerBoidPosition, tooCloseObjects):
     vec2 = (0, 0)
+    for obj in tooCloseObjects:
+        vec2 = vm.subVector2(vec2,vm.subVector2(obj.position,centerBoidPosition))
+    return vec2
+
 
 
 def separation( tooCloseObjects):
